327d4b701893a18944d73a0bfa439bae5175372a
Izael R Silva <87791471+IzaelRS@users.noreply.github.com>
2022-06-29 22:30:38 -0300

    Add files via upload

---
+++ eventos_de_mouse/script.js
@@ -0,0 +1,39 @@
+// Apertouo o botao onmousedown
+
+function apertoumouse() {
+    console.log("O mouse foi precionado");
+}
+
+
+// Soutei o botao onmouseup
+
+function solteimouse() {
+    console.log(" Eu soltei o mouse");
+}
+
+//passei o mouse em cima onmouseover
+
+function passeiomouseemcima() {
+    console.log(" estou passando o mouse em cima do botão ");
+}
+
+//tirei o mouse de cima onmouseout
+
+function tireiomousedecima() {
+    console.log(" eu tirei o mouse de cima");
+}
+
+// voce esta passando o mouse em cima do botão 
+function movendoomouseemcimadobotao() {
+    console.log(" Voce esta passando o mouse só em cima do botão ");
+}
+
+//onclick
+function onClick() {
+    console.log("Voce clicou no botão");
+}
+
+//oncontextmenu interagindo com o botão dirteito do mouse. essa funcao da para bloquear o botao direito com o return
+function botaodireito() {
+    console.log("Voce clicou com o botao direio do mouse")
+}
